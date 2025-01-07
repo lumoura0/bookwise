a96c1de5e8e913479e8630d7b535a7f04ecc3e7b
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-01-07 14:28:41 -0300

    creating connection with sqlite

--- dados.php
+++ dados.php
@@ -1,9 +1,17 @@
 <?php
-$livros = [
-    ['id' => 1, 'titulo' => 'Senhor dos Anéis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A terra Média'],
-    ['id' => 2, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'O segredo do sucesso'],
-    ['id' => 3, 'titulo' => 'A Cidade dos Sonhos', 'autor' => 'Miguel de Cervantes', 'descricao' => 'Uma obra que mudou a vida de muitos'],
-    ['id' => 4, 'titulo' => 'O Senhor dos Anéis - A Sociedade do Anel', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A terra Média'],
-    ['id' => 5, 'titulo' => 'A Odiseia', 'autor' => 'Homero', 'descricao' => 'Uma obra que mudou a vida de muitos'],
-    ['id' => 6, 'titulo' => 'O Pequeno Príncipe', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A terra Média'],
-];
+
+$db = new PDO('sqlite:database.sqlite');
+
+$query = $db->query("select * from livros");
+
+$livros = $query->fetchAll();
+
+
+// $livros = [
+//     ['id' => 1, 'titulo' => 'Senhor dos Anéis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A terra Média'],
+//     ['id' => 2, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'O segredo do sucesso'],
+//     ['id' => 3, 'titulo' => 'A Cidade dos Sonhos', 'autor' => 'Miguel de Cervantes', 'descricao' => 'Uma obra que mudou a vida de muitos'],
+//     ['id' => 4, 'titulo' => 'O Senhor dos Anéis - A Sociedade do Anel', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A terra Média'],
+//     ['id' => 5, 'titulo' => 'A Odiseia', 'autor' => 'Homero', 'descricao' => 'Uma obra que mudou a vida de muitos'],
+//     ['id' => 6, 'titulo' => 'O Pequeno Príncipe', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A terra Média'],
+// ];
--- views/livro.view.php
+++ views/livro.view.php
@@ -3,7 +3,7 @@
     <div class="flex">
         <div class="w-1/3">Imagem</div>
         <div class="space-y-1">
-            <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+            <a href="/livro?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
             <div class="text-xs italic"><?= $livro['autor'] ?></div>
             <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliaçôes)</div>
         </div>
